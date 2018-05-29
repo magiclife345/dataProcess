59d233d38e2e02b68d40e7bedbf163228c352d54
magiclife345 <magiclife345@hotmail.com>
2018-05-29 10:41:05 +0800

    first commit

---
+++ DataProcessPrj/src/main/DataProcess.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author magic
+ *
+ */
+public class DataProcess {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
